c0ce741dccbd1946a7c105a907ad06312cf5d5de
princelewis <princelewis4christ@yahoo.com>
2021-04-09 17:02:38 +0100

    Initial commit

---
+++ src/main/java/com/threeLine/atmcardverifier/AtmCardVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.threeLine.atmcardverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmCardVerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmCardVerifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threeLine/atmcardverifier/AtmCardVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeLine.atmcardverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmCardVerifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
