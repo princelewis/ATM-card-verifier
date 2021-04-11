c384d4fb4b213ca159fc9d9db4e4d4b779830c4c
princelewis <princelewis4christ@yahoo.com>
2021-04-11 14:45:50 +0100

    Create a util class for hashing token used to compare with the incoming token from header

---
+++ src/main/java/com/threeLine/atmcardverifier/util/TokenHasher.java
@@ -0,0 +1,39 @@
+package com.threeLine.atmcardverifier.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.util.encoders.Base64;
+
+import java.security.MessageDigest;
+
+@Slf4j
+public class TokenHasher {
+
+    public static String getHashedToken(String appKey, String timeStamp) {
+
+        String password = appKey + timeStamp;
+        String dataString = "";
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            md.update(password.getBytes());
+
+            byte[] dataEncrypt = md.digest();
+
+
+            dataString = new String(Base64.encode(dataEncrypt));
+
+        } catch (Exception ex) {
+            log.error("An error occurred while hashing token with error message as ****  {}", ex.getMessage(), ex);
+
+        }
+
+        return dataString;
+
+    }
+
+    public static void main(String[] args) {
+        String appKey = "hello_world";
+        System.out.println(getHashedToken(appKey,"87"));
+    }
+
+}
