9805c50a7d136b7490c62f961290ffb1a95350fd
princelewis <princelewis4christ@yahoo.com>
2021-04-12 00:33:35 +0100

    Create an Audit class

---
+++ src/main/java/com/threeLine/atmcardverifier/model/audit/DateAudit.java
@@ -0,0 +1,21 @@
+package com.threeLine.atmcardverifier.model.audit;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.Instant;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class DateAudit {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+}
