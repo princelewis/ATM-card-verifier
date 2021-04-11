c11b76b720f0c9d50c215949295efc7c6b4e5f90
princelewis <princelewis4christ@yahoo.com>
2021-04-11 14:36:27 +0100

    Configure Spring security to authenticate

---
+++ src/main/java/com/threeLine/atmcardverifier/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.threeLine.atmcardverifier.config;
+
+import com.threeLine.atmcardverifier.security.SecurityAuthenticationEntryPoint;
+import com.threeLine.atmcardverifier.security.TokenAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+/* This is to customize the default security system of springboot.
+Here we set the nature of urls that can be accessed without authentication
+* */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    SecurityAuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter();
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+        http.
+                exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favico.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.css",
+                        "/**/*.html",
+                        "/**/*.js")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        //Here we add TokenAuthenticationFilter before we call the authenticated method.
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+}
